5b4764cf6a536369ef132e68bd4c6485b790bad0
kosadmin <info@koutsourcing.vn>
2025-08-25 15:24:13 +0700

    Create sw.js
    Thêm và đăng ký Service Worker
    Để website trên Vercel hỗ trợ "Add to Home Screen" dưới dạng ứng dụng (standalone app) thay vì chỉ shortcut

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+        'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
